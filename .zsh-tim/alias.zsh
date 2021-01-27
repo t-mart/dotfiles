820010c7678b0c4a9aec0236e22853a27c6df784
Tim Martin <timothy_r_martin@homedepot.com>
2021-01-27 12:08:54 -0600

    Add dfconfig alias

--- .zsh-tim/alias.zsh
+++ .zsh-tim/alias.zsh
@@ -1,3 +1,5 @@
 if [ -x '/Applications/VLC.app/Contents/MacOS/VLC' ]; then
     alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
 fi
+
+alias dfconfig='git --git-dir=$HOME/.dotfile_config/ --work-tree=$HOME'
