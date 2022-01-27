fb44a70b557452a253487d038423ac3b687ea70d
Tim Martin <tim@timmart.in>
2022-01-27 02:04:17 -0600

    Add yt-dlp-beet-import parallel execution support

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,10 +4,11 @@ function dfgit {
 
 # takes a playlist url, like "https://music.youtube.com/playlist?list=OLAK5uy_lm79pWsUmJcnAPQdRZd9X41Ywv7m79cC0"
 function yt-dlp-beet-import {
-    New-Item -Name ".ytdlpbeetimport" -ItemType "directory"
-    yt-dlp --output ".ytdlpbeetimport/%(playlist)s/%(title)s.%(ext)s" --extract-audio --format "bestaudio*" "$args"
-    beet import ".ytdlpbeetimport"
-    Remove-Item -LiteralPath ".\.ytdlpbeetimport\" -Force -Recurse
+    $dirName = ".ytdlpbeetimport-$(Get-Date -Format yyyy-MM-dd-HH-mm-ss-fffffff)"
+    New-Item -Name "$dirName" -ItemType "directory"
+    yt-dlp --output "$dirName/%(playlist)s/%(title)s.%(ext)s" --extract-audio --format "bestaudio*[acodec=opus]/bestaudio*" "$args"
+    beet import "$dirName"
+    Remove-Item -LiteralPath ".\$dirName\" -Force -Recurse
 }
 
 Invoke-Expression (&starship init powershell)
