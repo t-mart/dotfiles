466bf0e3ed95d00195869daf0aaf5feee63b8640
Tim Martin <tim@timmart.in>
2024-04-28 22:23:26 -0500

    Make movefile script more robust

--- mpv.net/scripts/movefile.lua
+++ mpv.net/scripts/movefile.lua
@@ -27,35 +27,51 @@ local msg = require('mp.msg')
 
 local move_dir_name = "!mpv_movefile"
 
+-- log a message to the console at level and and display it to the user in the
+-- UI. the level may be nil, which will log at "info" level, or it may be any of
+-- `fatal`, `error`, `warn`, `info`, `v`, `debug`, or `trace`.
+local function log_and_osd_message(msg, level)
+    local level = level or 'info'
+    msg.log(level, msg)
+    mp.osd_message(msg)
+end
+
 -- Check if path is a protocol, such as `http://...`
-local function is_protocol(path) return path:match('^%a[%a%d-_]+://') end
+local function is_protocol(path)
+    return type(path) == 'string' and (path:match('^%a[%a%d_-]+://'))
+end
 
 -- remove the currently playing file from the playlist (which stops it from
 -- being played), or stop the player. this ensures we are no longer reading that
 -- file when we move it.
+--
+-- returns the result of the mp.command_native call (either a table on success,
+-- or (def, error) on error)
 local function playlist_remove_or_stop()
     local playlist_count = mp.get_property_native('playlist-count')
     if playlist_count > 1 then
-        mp.commandv('playlist-remove', 'current')
+        return mp.command_native({
+            name = 'playlist-remove',
+            index = 'current',
+        })
     else
-        mp.commandv('stop')
+        return mp.command_native({
+            name = 'stop',
+        })
     end
 end
 
 -- move a file from src to dst. returns true on success, or (false,
 -- error_message) on failure
 local function move_file(src, dst)
-    local success, error_message = os.rename(src, dst)
-    if not success then
-        return false, "Failed to move file: " .. error_message
-    end
-    return true
+    return os.rename(src, dst)
 end
 
--- create a directory at path. returns true on success, or (false,
--- error_message) on failure
+-- create a directory at path. returns true on success (if creation was
+-- successful or the directory already exists), or (false, error_message) on
+-- failure
 local function create_directory(path)
-    local info, error_message = utils.file_info(path)
+    local info = utils.file_info(path)
     if info then
         if info.is_dir then
             -- Path already exists and is a directory
@@ -66,21 +82,19 @@ local function create_directory(path)
         end
     else
         -- Path does not exist, try to create it
-        local success, error_message = os.execute('mkdir "' .. path .. '"')
-        if not success then
-            return false, "Failed to create directory: " .. error_message
+        -- local success, error_message = os.execute('mkdir "' .. path .. '"')
+        local result = mp.command_native({
+            name = 'subprocess',
+            args = {'mkdir', path},
+            playback_only = false,
+            capture_stdout = true,
+            capture_stderr = true,
+        })
+        if result.status ~= 0 then
+            return false, "Failed to create directory: " .. result.stderr
         end
+        return true
     end
-    return true
-end
-
--- log a message to the console at level and and display it to the user in the
--- UI. the level may be nil, which will log at "info" level, or it may be any of
--- `fatal`, `error`, `warn`, `info`, `v`, `debug`, or `trace`.
-local function log_and_osd_message(msg, level)
-    local level = level or 'info'
-    msg.log(level, msg)
-    mp.osd_message(msg)
 end
 
 -- skip the currently playing file and move it to the "!mpv_movefile" subdirectory
@@ -96,7 +110,11 @@ local function move_file_next()
     local move_dir_path = utils.join_path(file_dir_path, move_dir_name)
     local target_path = utils.join_path(move_dir_path, file_name)
 
-    playlist_remove_or_stop()
+    local pl_result, pl_error = playlist_remove_or_stop()
+    if pl_error then
+        log_and_osd_message("Failed to remove file from playlist: " .. pl_error, 'error')
+        return
+    end
 
     local success, error_message = create_directory(move_dir_path)
     if not success then
@@ -110,7 +128,7 @@ local function move_file_next()
         return
     end
 
-    mp.osd_message("Moved file to" .. target_path)
+    mp.osd_message("Moved file to " .. target_path)
 end
 
 -- make the function available for use.
