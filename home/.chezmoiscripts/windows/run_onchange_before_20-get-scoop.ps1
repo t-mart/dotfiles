5edabc8d56a09b6e8336a9df7bb8ceb30355c92e
Tim Martin <tim@timmart.in>
2025-07-10 05:50:34 -0500

    moar

--- home/.chezmoiscripts/windows/run_onchange_before_20-get-scoop.ps1
+++ home/.chezmoiscripts/windows/run_onchange_before_20-get-scoop.ps1
@@ -1,6 +1,16 @@
+Function Test-IsAdministrator {
+    $currentUser = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($currentUser)
+    return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
 if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
     Write-Host "scoop not found, installing..."
-    irm get.scoop.sh | iex
+    if (-not (Test-IsAdministrator)) {
+        irm get.scoop.sh | iex
+    } else {
+        iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+    }
 } else {
     Write-Host "scoop is already installed. Skipping installation."
 }
--- home/.chezmoiscripts/windows/run_onchange_before_21-foo.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-
-# print all os.environ variables in json
-
-print(json.dumps(dict(os.environ), indent=2))
-
-import sys
-sys.exit(1)
