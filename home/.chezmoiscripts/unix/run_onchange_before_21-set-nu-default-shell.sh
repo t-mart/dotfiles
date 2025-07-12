cda61eabecafd1942d377063743a308a8c60918c
Tim Martin <tim@timmart.in>
2025-07-12 14:52:58 -0500

    elevate

--- home/.chezmoiscripts/unix/run_onchange_before_21-set-nu-default-shell.sh
+++ home/.chezmoiscripts/unix/run_onchange_before_21-set-nu-default-shell.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+readonly ELEVATE_PREFIX=$([ "$EUID" -ne 0 ] && echo "sudo")
+
 # Find the path to the nushell executable. Exit if not found.
 readonly nu_path="$(which nu)" || { echo "nushell not found. Skipping set of it as default shell." >&2; exit 1; }
 
 # If /etc/shells exists, add nushell to it if it's not already present.
 if [ -f /etc/shells ]; then
-  grep -qxF "$nu_path" /etc/shells || echo "$nu_path" | sudo tee -a /etc/shells >/dev/null
+  grep -qxF "$nu_path" /etc/shells || echo "$nu_path" | $ELEVATE_PREFIX tee -a /etc/shells >/dev/null
 fi
 
 # Change the current user's shell to nushell.
