7bb59c6810b23f19cf4cd5eb860fc7cdd0adf553
Tim Martin <tim@timmart.in>
2025-07-17 15:40:42 -0500

    promote winget in path

--- home/.chezmoiscripts/windows/run_before_30-add-paths.ps1
+++ home/.chezmoiscripts/windows/run_before_30-add-paths.ps1
@@ -1,11 +1,12 @@
+# consider that this is an ordered list before modification
 [string[]]$PathsToAdd = @(
     "$env:USERPROFILE\.local\bin",
     "$env:USERPROFILE\.cargo\bin",
+    "$env:LOCALAPPDATA\Microsoft\WinGet\Links",
     "$env:USERPROFILE\scoop\shims",
     "$env:USERPROFILE\.local\share\pnpm",
     "$env:ProgramFiles\Docker\Docker\resources\bin",
-    "$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin",
-    "$env:LOCALAPPDATA\Microsoft\WinGet\Links"
+    "$env:LOCALAPPDATA\Programs\Microsoft VS Code\bin"
 )
 
 $User = [System.EnvironmentVariableTarget]::User
