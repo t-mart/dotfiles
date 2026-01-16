75d50fcbe52f5409d3c94b0f10282d1c2031ee0e
Tim Martin <tim@timmart.in>
2026-01-16 05:01:18 -0600

    fix path

--- home/.chezmoiscripts/unix/run_onchange_after_00-import-gpg-keys.sh
+++ home/.chezmoiscripts/unix/run_onchange_after_00-import-gpg-keys.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-"${CHEZMOI_EXECUTABLE}" decrypt home/.data/gpg-keys.txt.age | gpg --import --batch --yes
+"${CHEZMOI_EXECUTABLE}" decrypt "${CHEZMOI_WORKING_TREE}/data/gpg-keys.txt.age" | gpg --import --batch --yes
 
 echo "GPG keys imported successfully."
