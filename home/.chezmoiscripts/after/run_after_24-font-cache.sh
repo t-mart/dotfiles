e0417ead03c011b0fb63d25d9ce3c79be39d70ee
Tim Martin <tim@timmart.in>
2026-01-25 02:27:53 -0600

    desktop stuff

--- home/.chezmoiscripts/after/run_after_24-font-cache.sh
+++ home/.chezmoiscripts/after/run_after_24-font-cache.sh
@@ -3,9 +3,13 @@ set -euo pipefail
 
 source "${CHEZMOI_SOURCE_DIR}/.chezmoiscripts/.common.sh"
 
+if ! is_graphical; then
+  exit 0
+fi
+
 log_banner "Rebuilding font cache"
 
-if ! command_exists fc-cache; then
+if command_exists fc-cache; then
   fc-cache -fv
   log_info "Font cache rebuilt successfully."
 else
