b8c60b2011dc3a6a149151943ef8aa61eb1ed16e
Tim Martin <tim@timmart.in>
2026-02-06 23:29:32 -0600

    fix linked configs

--- home/.chezmoiscripts/after/run_after_23-thinkpad.sh
+++ home/.chezmoiscripts/after/run_after_23-thinkpad.sh
@@ -3,20 +3,43 @@ set -euo pipefail
 
 source "${CHEZMOI_SOURCE_DIR}/.chezmoiscripts/.common.sh"
 
+# Link a config file to a system location and show setup instructions if needed
+# Args: 
+# - name: used in messages
+# - target: the path of the target
+# - link_path: the link to create
+# - instructions_message: things to do next, shown if the link was updated
+link_config() {
+  local name=$1
+  local target=$2
+  local link=$3
+  local instructions=$4
+
+  local target
+  target=$(realpath "${target}")
+  
+  if [[ ! -f "$target" ]]; then
+    log_error "${name} config file not found at $target"
+    exit 1
+  fi
+
+  if is_softlink_of "$target" "$link"; then
+    log_info "${name} config link already correct."
+  else
+    log_info "Linking $link to ${target}..."
+    sudo ln -sf "$target" "$link"
+    gum confirm "$instructions"
+  fi
+}
+
 if is_thinkpad_z13; then
-
-  # TODO: this linking might now work! test with docker to ensure.
-
   log_banner "Configuring ThinkPad Z13 specific settings"
 
-  thinkfan_target="${CHEZMOI_SOURCE_DIR}/thinkfan.conf"
-  thinkfan_link="/etc/thinkfan.conf"
-  if is_softlink_of "$thinkfan_link" "$thinkfan_target"; then
-    log_info "Thinkfan config link already correct."
-  else
-    log_info "Linking $thinkfan_link to thinkfan config..."
-    sudo ln -sf "$thinkfan_target" "$thinkfan_link"
-    gum confirm "$(cat <<EOF
+  link_config \
+    "thinkfan" \
+    "${CHEZMOI_SOURCE_DIR}/dot_config/thinkfan.conf" \
+    "/etc/thinkfan.conf" \
+    "$(cat <<EOF
 Next, you need to:
 
 1. Run \`sensors-detect\`
@@ -25,21 +48,16 @@ Next, you need to:
 See https://wiki.archlinux.org/title/Fan_speed_control (especially the thinkfan section) for details.
 EOF
 )"
-  fi
 
-  keyboard_backlightd_target="${CHEZMOI_SOURCE_DIR}/keyboard-backlightd"
-  keyboard_backlightd_link="/etc/conf.d/keyboard-backlightd"
-  if is_softlink_of "$keyboard_backlightd_link" "$keyboard_backlightd_target"; then
-    log_info "keyboard-backlightd config link already correct."
-  else
-    log_info "Linking $keyboard_backlightd_link to keyboard-backlightd config..."
-    sudo ln -sf "$keyboard_backlightd_target" "$keyboard_backlightd_link"
-    gum confirm "$(cat <<EOF
+  link_config \
+    "keyboard-backlightd" \
+    "${CHEZMOI_SOURCE_DIR}/dot_config/keyboard-backlightd" \
+    "/etc/conf.d/keyboard-backlightd" \
+    "$(cat <<EOF
 Next, you need to enable and start the keyboard-backlightd service.
 
 See https://github.com/VorpalBlade/keyboard-backlightd for details.
 EOF
 )"
-  fi
 fi
 
